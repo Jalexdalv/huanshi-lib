4ce8a47b5809117b0582f635b19210e33b09b715
Jalexdalv <gaoyihang@aliyun.com>
2023-02-11 12:39:16 +0800

    提交更新

---
+++ src/main/java/org/huanshi/mc/lib/listener/PlayerJoinListener.java
@@ -0,0 +1,27 @@
+package org.huanshi.mc.lib.listener;
+
+import net.kyori.adventure.util.TriState;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.huanshi.mc.lib.annotation.Listener;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * PlayerJoinEvent 事件监听器
+ * @author Jalexdalv
+ */
+@Listener
+public class PlayerJoinListener extends AbstractListener {
+    /**
+     * PlayerJoinEvent 事件发生时处理
+     * @param playerJoinEvent PlayerJoinEvent 事件
+     */
+    @EventHandler
+    public void onPlayerJoinEvent(@NotNull PlayerJoinEvent playerJoinEvent) {
+        Player player = playerJoinEvent.getPlayer();
+        player.setFlyingFallDamage(TriState.FALSE);
+        player.setWalkSpeed(0.2F);
+        player.setCollidable(true);
+    }
+}
